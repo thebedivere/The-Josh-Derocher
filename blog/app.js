c68346f2d12410efd3da65299550b9a197465e84
Joshua Derocher <joshua.aarond@gmail.com>
2016-01-07 16:22:48 -0500

    Basic blog functions
    Basic blog functions are working. Posts and pages are currently in a
    couple of JSON files.

--- blog/app.js
+++ blog/app.js
@@ -3,4 +3,17 @@
 angular.module('app', [
     'ngRoute',
     'ngSanitize'
-]);
+]).config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostController'
+        }).when('/post/:id', {
+        templateUrl: 'views/singlepost.html',
+        controller: 'SinglePostController'
+    }).when('/page/:id', {
+        templateUrl: 'views/page.html',
+        controller: 'PageController'
+    }).otherwise({
+        redirectTo: '/'
+    })
+}]);
--- blog/main.ctrl.js
+++ blog/main.ctrl.js
@@ -1,4 +1,22 @@
-angular.module('app').controller("MainController", function () {
+angular.module('app')
+    .controller("MainController", function () {
     var vm = this;
     vm.title = 'The Coding Adventures of Josh Derocher';
-});
+    })
+    .controller('PostController', ['$scope', '$http', function($scope, $http){
+		$http.get('data/posts.json').success(function(data){
+			$scope.posts = data;
+			
+		});
+	}])
+	.controller('PageController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http){
+		$http.get('data/pages.json').success(function(data){
+			$scope.page = data[$routeParams.id];
+		});
+		
+	}])
+	.controller('SinglePostController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+		$http.get('data/posts.json').success(function(data){
+			$scope.post = data[$routeParams.id];
+		});
+	}]);
