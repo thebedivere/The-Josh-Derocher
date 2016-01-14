017e2d9d89ad46ccc6f4dac6ea78e55595cce115
Joshua Derocher <joshua.aarond@gmail.com>
2016-01-14 18:55:35 -0500

    Style is started.

---
+++ gruntfile.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = function (grunt) {
+
+
+    require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    './testApp/public/css/main.css': './sass/main.scss'
+                }
+            }
+        },
+        watch: {
+            sass: {
+                files: ['**/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false
+                },
+            },
+        }
+    });
+    grunt.registerTask('default', ['sass', 'watch']);
+};
