6d5b6441ad7a1c8fc18d8c78f014ba9f903a870f
Joshua Derocher <joshua.derocher@hds.com>
2016-03-21 13:05:42 -0400

    I can now edit blog posts, and link to a post from it's id.

--- gruntfile.js
+++ gruntfile.js
@@ -32,10 +32,11 @@ module.exports = function (grunt) {
                 src: [
 
                     'public/components/jquery/dist/jquery.js',
-                    'public/components/angular/angular.js',
-                    'public/components/showdown/dist/showdown.min.js',
+                    'public/components/jquery-ui/jquery-ui.js',
+                    'public/components/angular/angular.annotated.js',
+                    'public/components/showdown/dist/showdown.js',
                     'public/components/angular-animate/angular-animate.js',
-                    'public/components/angular-route/angular-route.js',
+                    'public/components/angular-route/angular-route.annotated.js',
                     'public/components/handlebars/handlebars.js'
                     /*
                     'public/*.js', 'public/home/*.js', 'public/blog/*.js', 'public/edit/*.js', 'src/js/*.js',
@@ -45,28 +46,45 @@ module.exports = function (grunt) {
                 dest: 'src/js/scripts.js'
             }
         },
+        ngAnnotate: {
+            options: {
+                singleQuotes: true,
+            },
+            app: {
+                files: [{
+                    expand: true,
+                    src: ['public/app.js', 'public/modules/**.js', 'public/components/angular-route/angular-route.js',  'public/components/angular/angular.js',],
+                    ext: '.annotated.js',
+                    extDot: 'last',
+                }],
+            },
+        },
         uglify: {
+            options: {
+                mangle: false
+            },
             my_target: {
                 files: {
                     'public/js/work.min.js': 'src/js/work.js', 
                     'public/js/resume.min.js': 'src/js/resume.js', 
                     'public/js/scripts.min.js': 'src/js/scripts.js', 
                     'public/js/animation.min.js': 'src/js/animation.js',
-                    'public/modules/modules.min.js': 'public/modules/**.js'
+                    'public/modules/modules.min.js': 'public/modules/**.annotated.js'
                 }
             }
         },
+        clean: ['public/**/*.min.js','public/**/*.annotated.js'],
         watch: {
             sass: {
                 files: ['**/*.scss'],
                 tasks: ['sass', 'cssmin']
             },
             scripts: {
-                files: ['public/**/*.js', '!public/**/**.min.js'],
-                tasks: ['concat', 'uglify']
+                files: ['public/**/*.js', 'public/modules/*.js', '!**/**.min.js', 'src/js/*.js'],
+                tasks: ['clean', 'ngAnnotate' ,'concat', 'uglify']
             },
         }
     });
-    grunt.registerTask('default', ['sass', 'cssmin', 'concat', 'uglify', 'watch']);
-    grunt.registerTask('js', ['concat', 'uglify']);
+    grunt.registerTask('default', ['clean', 'ngAnnotate', 'concat', 'sass', 'cssmin', 'concat', 'uglify', 'watch']);
+    grunt.registerTask('js', ['clean','ngAnnotate','concat', 'uglify']);
 };
